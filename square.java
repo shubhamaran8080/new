2f32ffa7bc2fbe2bfba383631b16f48c7cdc8e7d
shubhamaran8080 <166980856+shubhamaran8080@users.noreply.github.com>
2026-03-09 23:06:24 +0530

    Add files via upload

---
+++ square.java
@@ -0,0 +1,11 @@
+public class square {
+    public static void main(String[] args) {
+        for(int i=1; i<=100; i++){
+            if(i==i*i){
+                continue;
+
+            }
+            System.out.println(i);
+        }
+    }
+}
